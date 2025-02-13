2a3ab6f7f11c4a3acd17578773c374055f094b44
sriram5156689 <muralisriram23@gmail.com>
2025-02-13 20:47:28 +0530

    react
    react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center', // Centers horizontally
+    alignItems: 'center', // Centers vertically
+    height: '100vh', // Full viewport height
+};
+
+return (
+  <div style={containerStyle}>
+      <p style={{ fontSize: '100px', color: 'WHITE' }}>
+          SRIRAM
+      </p>
+  </div>
+  )
+}
+
+export default App
