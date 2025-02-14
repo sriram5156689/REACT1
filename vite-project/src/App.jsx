7dcef9007e9f8a18d69cff68d01f698e54780eb2
sriram5156689 <muralisriram23@gmail.com>
2025-02-14 22:56:12 +0530

    upadate
    day2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,19 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
+  alert ("my name is sriram");
+  const name ={
+    name:"sriram",
+    age:19,
+  };
   const containerStyle = {
     display: 'flex',
     justifyContent: 'center', // Centers horizontally
     alignItems: 'center', // Centers vertically
     height: '100vh', // Full viewport height
 };
+console.log("age="+name.age);
 
 return (
   <div style={containerStyle}>
+    <div className='a'>
       <p style={{ fontSize: '100px', color: 'WHITE' }}>
-          SRIRAM
+          <h1>name:{name.name}</h1>
       </p>
+    </div>
   </div>
   )
 }
